a166b868906466e9110610a460fcf06386942e32
Nagesh Potdar <nppotdar@gmail.com>
2013-09-22 17:48:54 -0400

    added custom src code to menu folder

---
+++ menu/nmt/restaurant/StartNMTScene.java
@@ -0,0 +1,17 @@
+package nmt.restaurant;
+
+import org.mt4j.MTApplication;
+
+public class StartNMTScene extends MTApplication {
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String args[]){
+		
+		initialize();
+	}
+	
+	@Override
+	public void startUp(){
+		this.addScene(new MainMenu(this, "Main Menu"));
+	}
+}
